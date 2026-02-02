69334c5b0e9430e5ffeb5b5d3fc5e168a74857da
Deep Malani <malanideep1008@gmail.com>
2026-02-02 15:20:00 +0530

    interview question by Pramod Ghodke Sir

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 dotenv.config();
-connectDB();
+
+const port = process.env.PORT || 5000;
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Not connected with DB, error is :", error);
+    });
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed:", error);
+  });
 
 // import express from "express";
 
